58d77acdb3e7dd92e5d2c5e55d878cdb8c4dc149
waynewwagiuw <s22110290@student.unklab.ac.id>
2025-12-09 19:56:00 +0800

    add responsif for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,25 +4,23 @@ export default function Navbar({ onOpen, onSearch }) {
   };
 
   return (
-    <>
-      <div className="navbar bg-base-100 shadow-sm p-4">
-        <div className="navbar-start">
-          <a className="btn btn-ghost text-xl">Clients</a>
-        </div>
-        <div className="navbar-center ">
-          <input
-            type="text"
-            placeholder="Search"
-            onChange={handleSearchChange}
-            className="input input-bordered w-48 md:w-auto"
-          />
-        </div>
-        <div className="navbar-end">
-          <a className="btn btn-primary" onClick={onOpen}>
-            Add Client
-          </a>
-        </div>
+    <div className="navbar bg-base-100 shadow-sm p-4 flex gap-3 md:flex-row md:justify-between">
+      <div className="flex justify-between w-full md:w-auto">
+        <a className="btn btn-ghost text-xl">Clients</a>
       </div>
-    </>
+      <div className="w-full md:w-1/2 flex justify-center">
+        <input
+          type="text"
+          placeholder="Search"
+          onChange={handleSearchChange}
+          className="input input-bordered w-full max-w-xs"
+        />
+      </div>
+      <div className="flex justify-end w-full md:w-auto">
+        <button className="btn btn-primary" onClick={onOpen}>
+          Add Client
+        </button>
+      </div>
+    </div>
   );
 }
